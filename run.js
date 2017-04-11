0e42545eef5044ef1b3f7be0d6054154b11302fa
Joseph Zhong <jzh@seas.upenn.edu>
2017-04-11 00:18:59 -0400

    make bot insult owner; add ownership

--- run.js
+++ run.js
@@ -8,42 +8,53 @@ const token = secrets.token;
 
 // Let's keep the commands separate to not clutter this file
 const commands = require('./commands.js');
+var botOwner = process.argv.slice(2);
 
 // Globals for current (last message received in) channel, and text received
 var channel;
-var text; 
+var text;
 
 client.on('ready', () => {
-  console.log('Shitty-Bot is online.');
-  // channel.sendMessage('Shitty-Bot is online.'); // Commented out b/c this is kind of annoying when testing
+    channel = client.channels.find('name', 'status');
+    console.log('Shitty-Bot is online.');
+    channel.sendMessage(`${botOwner}\'s Shitty-Bot is online.`);
+});
+
+client.on('disconnect', () => {
+    channel = client.channels.find('name', 'status');
+    console.log('Shitty-Bot is disconnecting');
+    channel.sendMessage(`${botOwner}\'s Shitty-Bot is disconnecting.`);
 });
 
 client.on('message', message => {
-  
-  // Log the conversation
-  console.log(message.channel.id + ' > ' + message.author.username + ' > ' + message.content);
-  
-  // Switch to the channel the last message was sent from
-  channel = client.channels.get(message.channel.id);
-  
-  // Let's use this for now, but maybe find a RegEx alternative for case insensitive string matching
-  text = message.content.toLowerCase();
-  
-  // Handle stock requests
-  if (text.includes('stock')) {
-    commands.stock(channel, text);
-  }
-  
-  // Respond to uptime request
-  if (text === 'uptime') {
-    channel.sendMessage(commands.uptime(client));
-  }
-  
-  // lol joe
-  if (text.includes('joe') && message.author.username != 'Shitty-Bot') {
-    commands.insult(channel);
-  }
-  
+
+    // Log the conversation
+    console.log(message.channel.id + ' > ' + message.author.username + ' > ' + message.content);
+
+    // Switch to the channel the last message was sent from
+    channel = client.channels.get(message.channel.id);
+
+    // Let's use this for now, but maybe find a RegEx alternative for case insensitive string matching
+    text = message.content.toLowerCase();
+
+    // Handle stock requests
+    if (text.includes('stock')) {
+        commands.stock(channel, text);
+    }
+
+    // Respond to uptime request
+    if (text === 'uptime') {
+        channel.sendMessage(commands.uptime(client));
+    }
+    // lol joe
+    if (text.includes(botOwner.toString().toLowerCase()) && message.author.username != 'Shitty-Bot') {
+        commands.insult(channel, botOwner.toString());
+    }
+
+    if (text === '/exit') {
+        commands.kill(client);
+    }
+
 });
 
 client.login(token);
