8dee2824bd798d2e4829e360acbb86559c2b4760
Joseph Zhong <jzh@seas.upenn.edu>
2017-04-11 00:32:34 -0400

    resolve merge conflicts; add default owner of Jeff

--- commands.js
+++ commands.js
@@ -25,52 +25,28 @@ module.exports = {
                 catch(err) {
                     channel.sendMessage('Unable to look up "' + ticker + '".');
                 }
-            },
+            })
+        }
+    },
 
-                // Uptime - tell me how long the bot's been online, or more importantly if it's actually online
-                uptime: function(client) {
-                    // To do: make this fancier and actually tell you what time the bot came online
-                    var uptime = client.uptime / 1000;
-                    return 'Online for ' + uptime + ' seconds.';
-                },
 
-                // Insult - generate an Elizabethan insult and send it
-                insult: function(channel, owner) {
-                    console.log('Retrieving an Elizabethan insult');
-                    request(insultURL, (error, response, body) => {
-                        if (!error && response.statusCode === 200) {
-                            var response = JSON.parse(body);
-                            channel.sendMessage(`${owner}, ` + response.insult);
-                        }
-                        else {
-                            channel.sendMessage('Got an error: ', error, ', status code: ', response);
-                        }
-                    })
-                },
+    // Tell me how long the bot's been online, or more importantly if it's actually online
+    uptime: function(client, startTime) {
+        var uptime = client.uptime / 1000;
+        return `Online for ${uptime} seconds since ${startTime.toUTCString()}.`;
+    },
 
-                kill: function(client) {
-                    client.destroy();
-                }
-        },
-
-            // Tell me how long the bot's been online, or more importantly if it's actually online
-            uptime: function(client, startTime) {
-                var uptime = client.uptime / 1000;
-                return `Online for ${uptime} seconds since ${startTime.toUTCString()}.`;
-            },
-
-            // generate an Elizabethan insult and send it (to joe)
-            insultJoe: function(channel) {
-                console.log('Retrieving an Elizabethan insult');
-                request(insultURL, (error, response, body) => {
-                    if (!error && response.statusCode === 200) {
-                        var response = JSON.parse(body);
-                        channel.sendMessage('Joe, ' + response.insult);
-                    }
-                    else {
-                        channel.sendMessage('Got an error: ', error, ', status code: ', response);
-                    }
-                })
+    // Insult - generate an Elizabethan insult and send it
+    insult: function(channel, owner) {
+        console.log('Retrieving an Elizabethan insult');
+        request(insultURL, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                var response = JSON.parse(body);
+                channel.sendMessage(`${owner}, ` + response.insult);
             }
-
-    }
+            else {
+                channel.sendMessage('Got an error: ', error, ', status code: ', response);
+            }
+        })
+    },
+}
--- run.js
+++ run.js
@@ -9,11 +9,14 @@ var botOwner = process.argv.slice(2);
 
 var channel;
 var text;
-<<<<<<< HEAD
+var startTime;
 
 client.on('ready', () => {
     channel = client.channels.find('name', 'status');
     console.log('Shitty-Bot is online.');
+    if (botOwner.toString() === ''){
+        botOwner = "Jeff";
+    }
     channel.sendMessage(`${botOwner}\'s Shitty-Bot is online.`);
 });
 
@@ -25,71 +28,29 @@ client.on('disconnect', () => {
 
 client.on('message', message => {
 
-    // Log the conversation
     console.log(message.channel.id + ' > ' + message.author.username + ' > ' + message.content);
 
-    // Switch to the channel the last message was sent from
-    channel = client.channels.get(message.channel.id);
+    channel = client.channels.get(message.channel.id); // Switch to the channel the last message was sent from
+    text = message.content.toLowerCase(); // To do: Find a better way to handle case sensitivity
 
-    // Let's use this for now, but maybe find a RegEx alternative for case insensitive string matching
-    text = message.content.toLowerCase();
-
-    // Handle stock requests
     if (text.includes('stock')) {
-        commands.stock(channel, text);
+        commands.getStockPrice(channel, text);
     }
 
-    // Respond to uptime request
     if (text === 'uptime') {
-        channel.sendMessage(commands.uptime(client));
+        channel.sendMessage(commands.uptime(client, startTime));
     }
+
     // lol joe
     if (text.includes(botOwner.toString().toLowerCase()) && message.author.username != 'Shitty-Bot') {
         commands.insult(channel, botOwner.toString());
     }
 
-    if (text === '/exit') {
-        commands.kill(client);
+    if (text === '/shutdown') {
+        channel.sendMessage('Shutting down.');
+        client.destroy();
     }
 
-=======
-var startTime;
-
-client.on('ready', () => {
-  startTime = new Date();
-  console.log('Shitty-Bot online');
-});
-
-client.on('disconnect', () => {
-  startTime = new Date();
-  console.log('Shitty-Bot offline');
-});
-
-client.on('message', message => {
-  
-  console.log(message.channel.id + ' > ' + message.author.username + ' > ' + message.content);
-  
-  channel = client.channels.get(message.channel.id); // Switch to the channel the last message was sent from
-  text = message.content.toLowerCase(); // To do: Find a better way to handle case sensitivity
-  
-  if (text.includes('stock')) {
-    commands.getStockPrice(channel, text);
-  }
-  
-  if (text === 'uptime') {
-    channel.sendMessage(commands.uptime(client, startTime));
-  }
-  
-  if (text.includes('joe') && message.author.username != 'Shitty-Bot') {
-    commands.insultJoe(channel);
-  }
-  
-  if (text === '/shutdown') {
-    channel.sendMessage('Shutting down.');
-    client.destroy(); 
-  }
-  
->>>>>>> 004d4359838142ba9babc96b1ac9cdfed7317941
 });
 
 client.login(token);
