f6153008e499aa557117b7cb36ab14e2bb4a070f
Jeffrey Yan <jyan@flatiron.com>
2017-04-11 11:22:25 -0400

    fixin shit

--- run.js
+++ run.js
@@ -5,7 +5,8 @@ const secrets = require('./secrets');
 const token = secrets.token;
 
 const commands = require('./commands.js');
-const stockPatt = /\$[a-z]{1,5}/gi;
+const stockPattern = /\$[a-z]{1,5}/gi;
+
 var botOwner = String(process.argv.slice(2));
 
 var channel;
@@ -15,7 +16,7 @@ var startTime;
 client.on('ready', () => {
     startTime = new Date();
     channel = client.channels.find('name', 'status');
-    console.log('Shitty-Bot is online.');
+    console.log(`${botOwner}\'s Shitty-Bot is online.`);
     if (botOwner === '') {
         botOwner = "Jeff";
     }
@@ -32,30 +33,35 @@ client.on('message', message => {
 
     console.log(message.channel.id + ' > ' + message.author.username + ' > ' + message.content);
 
+    if (message.author.username === 'Shitty-Bot') return; // Ignore messages sent 
     channel = client.channels.get(message.channel.id); // Switch to the channel the last message was sent from
-    var text = '';
-    if (message.author.username != 'Shitty-Bot') {
-        text = message.content.toLowerCase(); // To do: Find a better way to handle case sensitivity
-    }
+    text = text = message.content.toLowerCase(); // To do: Find a better way to handle case sensitivity
 
     var match;
-    while ((match = stockPatt.exec(text)) !== null){
+    while ((match = stockPattern.exec(text)) !== null){
         console.log(`found ${match[0]}`);
         commands.getStockPrice(channel, match[0]);
     }
 
+    // Shutdown needs to go first, if shutdown is called ignore everything else
+    // To do: fix that damn error message when we shut down
     if (text === '/shutdown ' + botOwner.toLowerCase()) {
         channel.sendMessage(`${botOwner}\'s Shitty-Bot Shutting down.`);
         client.destroy();
     }
+
     else if (text === 'uptime') {
         channel.sendMessage(commands.uptime(client, startTime));
+    }
+
+    else if (text.includes('stock')) {
+        text = '$' + text.replace('stock', '').replace(' ', '');
+        if (text.length < 6) commands.getStockPrice(channel, text);
     }
 
     else if (text.includes(botOwner.toString().toLowerCase())) {
         commands.insult(channel, botOwner);
     }
-
 
 });
 
