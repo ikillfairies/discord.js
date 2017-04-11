f31b005c7b80830e04d6b0eeb179fbb1fa744a9e
Jeffrey Yan <jyan@flatiron.com>
2017-04-11 00:11:58 -0400

    shutdown and stuff

--- commands.js
+++ commands.js
@@ -1,24 +1,19 @@
-const request = require('request'); // Used for JSON parsing
+const request = require('request');
 const stockURL = 'http://finance.google.com/finance/info?client=ig&q=';
 const insultURL = 'http://quandyfactory.com/insult/json';
 
 // Note: You must pass channel in explicitly when doing an asynch JSON request
 module.exports = {
   
-  // Stock - see http://www.jarloo.com/real-time-google-stock-api/ for API documentation
-  stock: function(channel, ticker) {
+  // See http://www.jarloo.com/real-time-google-stock-api/ for API documentation
+  // To do: Get a better API, this one blows
+  getStockPrice: function(channel, ticker) {
     ticker = ticker.replace(' ', '');
     ticker = ticker.replace('stock', '');
     ticker = ticker.toUpperCase();
-    // Ignore the keyword 'stock' if the phrase is longer than 5 characters.
     if (ticker.length < 6) {
-      // Log the request to the console for debugging purposes
-      console.log('Stock request received for: ' + ticker);
-      // Request the stock information from the API in JSON format
       request(stockURL + ticker, (error, response, body) => {
-        // Wrap the request in a try because bitches might pass invalid tickers
         try {
-          // I need to clean the string, why the fuck does this API format their shit so weird?
           var parsedBody = body.split('[')[1].split(']')[0];
           if (!error && response.statusCode === 200) {
             var response = JSON.parse(parsedBody);
@@ -35,15 +30,14 @@ module.exports = {
     }
   },
   
-  // Uptime - tell me how long the bot's been online, or more importantly if it's actually online
+  // Tell me how long the bot's been online, or more importantly if it's actually online
   uptime: function(client, startTime) {
-    // To do: make this fancier and actually tell you what time the bot came online
     var uptime = client.uptime / 1000;
     return `Online for ${uptime} seconds since ${startTime.toUTCString()}.`;
   },
   
-  // Insult - generate an Elizabethan insult and send it (to joe)
-  insult: function(channel) {
+  // generate an Elizabethan insult and send it (to joe)
+  insultJoe: function(channel) {
     console.log('Retrieving an Elizabethan insult');
     request(insultURL, (error, response, body) => {
       if (!error && response.statusCode === 200) {
--- run.js
+++ run.js
@@ -1,51 +1,47 @@
-// Create the Discord bot
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-// Shitty-Bot's login credentials
 const secrets = require('./secrets');
 const token = secrets.token;
 
-// Let's keep the commands separate to not clutter this file
 const commands = require('./commands.js');
 
-// Globals for current (last message received in) channel, and text received
 var channel;
 var text;
-
-// Global for bot start time
 var startTime;
 
 client.on('ready', () => {
   startTime = new Date();
-  console.log('Shitty-Bot is online.');
-  // channel.sendMessage('Shitty-Bot is online.'); // Commented out b/c this is kind of annoying when testing
+  console.log('Shitty-Bot online');
+});
+
+client.on('disconnect', () => {
+  startTime = new Date();
+  console.log('Shitty-Bot offline');
 });
 
 client.on('message', message => {
   
-  // Log the conversation
   console.log(message.channel.id + ' > ' + message.author.username + ' > ' + message.content);
   
-  // Switch to the channel the last message was sent from
-  channel = client.channels.get(message.channel.id);
+  channel = client.channels.get(message.channel.id); // Switch to the channel the last message was sent from
+  text = message.content.toLowerCase(); // To do: Find a better way to handle case sensitivity
   
-  // Let's use this for now, but maybe find a RegEx alternative for case insensitive string matching
-  text = message.content.toLowerCase();
-  
-  // Handle stock requests
   if (text.includes('stock')) {
     commands.stock(channel, text);
   }
   
-  // Respond to uptime request
   if (text === 'uptime') {
     channel.sendMessage(commands.uptime(client, startTime));
   }
   
-  // lol joe
   if (text.includes('joe') && message.author.username != 'Shitty-Bot') {
     commands.insult(channel);
+  }
+  
+  if (text === '/shutdown') {
+    channel.sendMessage('Shutting down.');
+    client.destroy(); 
   }
   
 });
